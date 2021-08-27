6c66bfa6d25932e0e5a49bd4a30c77586cbddd9d
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-08-27 18:59:38 -0300

    Portfolio update

--- portfolio-victor-santiago/src/pages/Projects.js
+++ portfolio-victor-santiago/src/pages/Projects.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import Header from '../components/Header';
+import '../App.css';
 import { Link } from 'react-router-dom';
-import { IoPizzaOutline, IoCaretBackCircleSharp } from 'react-icons/io5';
+import { IoPizzaOutline, IoCaretBackCircleSharp, IoGameControllerOutline, IoCashOutline } from 'react-icons/io5';
 
 export default function Projects() {
   return (
     <div>
       <Header />
-      <Link to='/'><IoCaretBackCircleSharp /></Link>
       <img src="https://octocat-generator-assets.githubusercontent.com/my-octocat-1628983230728.png" alt="myOcto" width="350px" height="350px" align="right"/>
-      <main>
+      <div className="divProjects">
+      <Link to='/'><IoCaretBackCircleSharp className='backIcon' /></Link>
         <ul>
           <li>
             <h2>
               <a href='https://github.com/visantiago99/Recipes-App' target='_blank' rel='noreferrer'>Recipes App <IoPizzaOutline /></a>
             </h2>
           </li>
+          <li>
+            <h2>
+              <a href='https://github.com/visantiago99/Trivia-App-React-Redux' target='_blank' rel='noreferrer'>Trivia App <IoGameControllerOutline /></a>
+            </h2>
+          </li>
+          <li>
+            <h2>
+              <a href='https://github.com/visantiago99/Wallet-Coin-Converter' target='_blank' rel='noreferrer'>Wallet and Coin Converter App <IoCashOutline /></a>
+            </h2>
+          </li>
         </ul>
-      </main>
+      </div>
     </div>
   )
 }
